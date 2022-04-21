d2e30cf2ef094dbc08cf8cb6937433df4da14835
djdenielb <luisdmateos@outlook.com>
2022-04-21 18:14:45 -0500

    Initial Commit

---
+++ 8AppAPIBasica/Modelo/CerebroApi.swift
@@ -0,0 +1,8 @@
+//
+//  CerebroApi.swift
+//  8AppAPIBasica
+//
+//  Created by djdenielb on 21/04/22.
+//
+
+import Foundation
---
+++ 8AppAPIBasica/Modelo/ModeloApi.swift
@@ -0,0 +1,10 @@
+//
+//  ModeloApi.swift
+//  8AppAPIBasica
+//
+//  Created by djdenielb on 21/04/22.
+//
+
+import Foundation
+
+
